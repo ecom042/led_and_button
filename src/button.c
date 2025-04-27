bd495f95f186cc75cb4d5854013ffb33a8c7cb30
Rafael Santana <rafaelsantanaps@gmail.com>
2025-04-27 18:51:58 -0300

    button: add long press event handling

--- src/button.c
+++ src/button.c
@@ -11,6 +11,7 @@ ZBUS_CHAN_DEFINE(chan_button_evt, struct msg_button_evt, NULL, NULL, ZBUS_OBSERV
 		 ZBUS_MSG_INIT(.evt = BUTTON_EVT_UNDEFINED));
 
 #define SLEEP_TIME_MS 1
+#define LONG_PRESS_THRESHOLD_MS 3000
 
 /*
  * Get button configuration from the devicetree sw0 alias. This is mandatory.
@@ -19,24 +20,47 @@ ZBUS_CHAN_DEFINE(chan_button_evt, struct msg_button_evt, NULL, NULL, ZBUS_OBSERV
 #if !DT_NODE_HAS_STATUS_OKAY(SW0_NODE)
 #error "Unsupported board: sw0 devicetree alias is not defined"
 #endif
+
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 static struct gpio_callback button_cb_data;
+static int64_t press_time_ms;
 
+/**
+ * @brief Callback function to handle button press and release events
+ *
+ * @param dev Device structure pointer
+ * @param cb Callback structure pointer
+ * @param pins GPIO pin mask
+ */
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
 	struct msg_button_evt msg = {.evt = BUTTON_EVT_UNDEFINED};
+	int64_t current_time = k_uptime_get();
 
 	if (gpio_pin_get_dt(&button)) {
 		msg.evt = BUTTON_EVT_PRESSED;
+		press_time_ms = current_time;
 		printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
 	} else {
 		msg.evt = BUTTON_EVT_RELEASED;
 		printk("Button released at %" PRIu32 "\n", k_cycle_get_32());
+
+		if ((current_time - press_time_ms) >= LONG_PRESS_THRESHOLD_MS) {
+			struct msg_button_evt long_press_msg = {.evt = BUTTON_EVT_LONGPRESS};
+			zbus_chan_pub(&chan_button_evt, &msg, K_NO_WAIT);
+			zbus_chan_pub(&chan_button_evt, &long_press_msg, K_NO_WAIT);
+			return;
+		}
 	}
 
 	zbus_chan_pub(&chan_button_evt, &msg, K_NO_WAIT);
 }
 
+/**
+ * @brief Initialize the button GPIO
+ *
+ * @return 0 on success, negative error code otherwise
+ */
 int button_init(void)
 {
 	int ret;
@@ -49,19 +73,24 @@ int button_init(void)
 	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 	if (ret != 0) {
 		printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name,
-		       button.pin);
+			button.pin);
 		return ret;
 	}
 
 	return 0;
 }
 
+/**
+ * @brief Enable interrupts for the button GPIO
+ *
+ * @return 0 on success, negative error code otherwise
+ */
 int button_enable_interrupts(void)
 {
 	int ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_BOTH);
 	if (ret != 0) {
 		printk("Error %d: failed to configure interrupt on %s pin %d\n", ret,
-		       button.port->name, button.pin);
+			button.port->name, button.pin);
 		return ret;
 	}
 
